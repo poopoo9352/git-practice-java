43ad7323763f35a883598d2ce910ef5635f8a067
Mahima <mahimasachdeva92@gmail.com>
2025-09-05 00:53:47 +0530

    add datatypes

---
+++ DataTypes.java
@@ -0,0 +1,32 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int myNumber = 865;
+        int yourNumber;
+        yourNumber = 456;
+        System.out.println(myNumber);
+        System.out.println(yourNumber);
+
+        boolean isVeg = true;
+        System.out.println(isVeg);
+
+        String kebab = ("veg");
+        System.out.println(kebab);
+
+        int a = 3;
+        int b = 2;
+        int sum = a+b;
+        System.out.println(sum);
+        System.out.println(sum+a);
+
+        String firstname = "james";
+        String lastname = "manchanda";
+        String name = firstname+" "+lastname;
+        System.out.println(name);
+
+        char Name = 'M';
+        System.out.println(Name);
+
+
+
+    }
+}
